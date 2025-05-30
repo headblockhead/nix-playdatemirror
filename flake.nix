c6f3353de248c8ea52776afca4b74ab9632f0856
Edward Hesketh <inbox@edwardh.dev>
2025-05-30 23:36:10 +0100

    feat: overhaul flake, update to version 1.3.0!

--- flake.nix
+++ flake.nix
@@ -1,50 +1,42 @@
 {
   description = "Mirror is an app that streams gameplay audio and video in real-time from your Playdate to a macOS, Windows, or Linux computer.";
 
-  outputs = { self, nixpkgs }: 
-  let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-    };
-    mirrorsrc = pkgs.callPackage ./mirror.nix {};
-    mirrorapp = pkgs.buildFHSUserEnv {
-      name = "Mirror";
-      targetPkgs = pkgs:
-      [
-          pkgs.wrapGAppsHook
-          pkgs.glib
-          pkgs.gdk-pixbuf
-          pkgs.cairo
-          pkgs.pango
-          pkgs.udev
-          pkgs.alsa-lib
-          pkgs.gtk3
-          pkgs.webkitgtk
-          pkgs.pkg-config
-          mirrorsrc
-          pkgs.xorg.libX11
-          pkgs.xorg.libXcursor
-          pkgs.xorg.libXrandr
-          pkgs.libserialport
-          pkgs.gnome.adwaita-icon-theme
-        ];
-        runScript = ''MirrorFromDownload'';
-    };
-    shell = pkgs.mkShell {
-      shellHook = ''
-        # Set the GSETTINGS_SCHEMA_DIR
-        export GSETTINGS_SCHEMA_DIR=${pkgs.glib.getSchemaPath pkgs.gtk3}
-        # Set the XDG_DATA_DIRS
-        export XDG_DATA_DIRS=$HOME/.nix-profile/share:/usr/local/share:/usr/share
-                '';
-      packages = [ 
-        mirrorsrc
-        mirrorapp
-      ];
-    };
-  in
-  {
-    packages.x86_64-linux.Mirror = mirrorapp;
-    defaultPackage.x86_64-linux = shell;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
+
+  outputs = { self, nixpkgs }:
+    with import nixpkgs { system = "x86_64-linux"; };
+    rec {
+      packages.x86_64-linux.Mirror = stdenv.mkDerivation rec {
+        version = "1.3.0";
+        name = "mirror-${version}";
+        src = fetchurl {
+          url = "https://download.panic.com/mirror/Linux/Mirror-${version}.tar.gz";
+          hash = "sha256-JQNpl5UKaoEpj5wvp6k2iZ7qyKXpWiIG/z8nHt0+l38=";
+        };
+        nativeBuildInputs = [ autoPatchelfHook ];
+
+        buildInputs = [
+          gtk3
+          webkitgtk
+        ];
+
+        installPhase = ''
+          runHook preInstall
+          tar xfz $src
+          mkdir -p $out/bin
+          cp Mirror-${version}/mirror $out/bin/mirror
+          runHook postInstall
+        '';
+
+        sourceRoot = ".";
+        meta = with lib; {
+          homepage = "https://play.date/mirror";
+          description = "Mirror streams gameplay audio and video in real-time from your Playdate to a computer";
+          platforms = platforms.linux;
+        };
+      };
+      packages.x86_64-linux.default = packages.x86_64-linux.Mirror;
+    };
 }
